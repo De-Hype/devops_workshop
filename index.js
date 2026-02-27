aff0a8b50d663eb347f42adc7283e3a27cf195ba
David Hype <iamsocialhype@gmail.com>
2026-02-27 21:24:57 +0100

    refactor: refactored the response sent on the home routes to include status and new message

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({message:"Great to have you guys here"})
+  res.json({
+    status:"success",
+    message:"Thank you for staying till now"})
 });
 
 app.listen(port, "0.0.0.0",() => {
